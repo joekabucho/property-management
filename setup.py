71f99f2a1ca9d04874e02aa05fae7dc775a14988
joe kabucho <joekabucho2@gmail.com>
2019-10-16 12:10:37 +0300

    req fix 2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in property/__init__.py
@@ -20,5 +20,7 @@ setup(
 	author_email='info@ossph.com',
 	packages=find_packages(),
 	zip_safe=False,
-	include_package_data=True
+	include_package_data=True,
+	install_requires=[str(ir.req) for ir in requirements],
+	dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
